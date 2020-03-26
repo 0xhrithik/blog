7ff73367813027003d8c8c3e7aab0e7007ad0905
hrithiklodha <hrithikjain909@gmail.com>
2020-03-27 04:36:35 +0530

    create each post template

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -38,6 +39,25 @@ app.post('/compose',function(req,res){
   res.redirect('/');
 });
 
+app.get('/posts/:postName',function(req,res){
+  const requestedTitle = lodash.lowerCase(req.params.postName);
+  posts.forEach(function(post){
+    const storedTitle = lodash.lowerCase(post.postTitle);
+    console.log(requestedTitle);
+    console.log(storedTitle);
+    if(requestedTitle===storedTitle){
+      // console.log('Match Found');
+      res.render('./post',{postTitle:post.postTitle,postBody:post.postBody});
+    }
+    // else{
+    //   res.redirect('/404');
+    // }
+  });
+})
+
+app.get('/post',function(req,res){
+
+});
 
 
 
